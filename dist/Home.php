be2bd8298b883764fa60862e0770f9de9cb6ca0c
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2024-02-28 16:30:26 +0530

    home

--- dist/Home.php
+++ dist/Home.php
@@ -4,8 +4,8 @@
     <!-- background img on landing page -->
     <div class="w-full lg:h-screen h-96 object-cover bg-gray-100 sm:block -z-30 overflow-hidden">
         <div class="relative flex justify-center items-center h-full z-10 bg-center bg-cover" style="background-image: url('../img/FMS-home.jpeg');">
-            <h1 class="text-black lg:text-7xl md:text-5xl text-3xl text-center sm:font-bold md:font-thin lg:font-semibold">"FARMING IS A PEOFESSION <br> OF HOPE" <br>
-                <span class="text-black lg:text-3xl md:text-xl text-lg font-bold">TAKING AGRICULTURE TO THE NEXT LEVEL</span>
+            <h1 class="text-slate-500 lg:text-7xl md:text-5xl text-3xl text-center sm:font-bold font-serif md:font-thin lg:font-semibold">"FARMING IS A PEOFESSION <br> OF HOPE" <br>
+                <span class="text-slate-500 lg:text-3xl md:text-xl text-lg font-bold font-serif">TAKING AGRICULTURE TO THE NEXT LEVEL</span>
                 <div class="w-full h-full text-center mt-5 ">
                     <a class="w-fit border-2 border-white bg-white font-bold rounded-md md:py-2 py-1 px-2 md:px-5 text-black text-lg md:text-2xl" href="Login.php">Get Started</a>
                 </div>
