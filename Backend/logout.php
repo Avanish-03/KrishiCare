0b066a12d3e33af1bad5f086fa5b0b6e1b09088a
Dwivedi <devweb910@gmail.com>
2024-03-05 12:09:11 +0530

    Changes done

--- Backend/logout.php
+++ Backend/logout.php
@@ -4,10 +4,6 @@ session_start();
 session_unset();
 session_destroy();
 
-<<<<<<< HEAD
-// header("location: ../dist/");
-=======
 // header("location:../dist/Home.php");
->>>>>>> 8632d4218f870894173fb42ade92ba49b5756ccf
 
 ?>   
--- farmer/FarmerDashboard.php
+++ farmer/FarmerDashboard.php
@@ -11,12 +11,8 @@ if ($_SESSION["farmer"] == false) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Farmer Dashboard</title>
-<<<<<<< HEAD
-    <link rel="stylesheet" href="../dist/output.css">
-=======
     <script src='https://cdnjs.cloudflare.com/ajax/libs/alpinejs/3.3.2/cdn.js'></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
->>>>>>> 8632d4218f870894173fb42ade92ba49b5756ccf
     <script src="../src/Js/code.js"></script>
 </head>
 
