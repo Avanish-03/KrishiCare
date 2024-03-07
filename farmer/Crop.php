30115ac2015e3d4040d96e0818c91dddfa184f7b
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2024-03-07 11:20:37 +0530

    crop

--- farmer/Crop.php
+++ farmer/Crop.php
@@ -2,7 +2,7 @@
 
 <!-- Crop Detail Section -->
 <section class="h-full container mx-auto mt-8 p-4 rounded-lg">
-  <h2 class="text-3xl font-semibold mb-4">Crop Detail</h2>
+  <h2 class="text-3xl font-semibold text-white mb-4">Crop Detail</h2>
 
   <!--1. wheat Crop Information -->
   <div id="wheat" class="mt-8">
@@ -11,17 +11,16 @@
         <img src="https://images.pexels.com/photos/162769/ear-cereals-infructescence-staple-food-162769.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Crop Image" class="w-full h-80 mb-4 rounded-lg">
       </div>
       <div>
-        <h3 class="text-xl font-semibold mb-2">Crop Name: Wheat</h3>
-        <p class="text-gray-700 mb-2">Variety: Winter Wheat</p>
-        <p class="text-gray-700 mb-2">Planting Season: October - November</p>
-        <p class="text-gray-700 mb-2">Harvesting Season: April - June</p>
-        <p class="text-gray-700 mb-2">Soil Type: Well-drained loamy soil</p>
-        <h3 class="text-2xl font-semibold mb-4">Crop Description</h3>
-        <p class="text-gray-700 mb-2"> Wheat is the main cereal crop in India. The total area under the crop is about 29.8 million hectares in the country. The production of wheat in the country has increased significantly from 75.81 million MT in 2006-07 to an all time record high of 94.88 million MT in 2011-12. The productivity of wheat which was 2602 kg/hectare in 2004-05 has increased to 3140 kg/hectare in 2011-12. </p>
+        <h3 class="text-xl font-semibold mb-2 text-white">Crop Name: Wheat</h3>
+        <p class="text-gray-100 mb-2">Variety: Winter Wheat</p>
+        <p class="text-gray-100 mb-2">Planting Season: October - November</p>
+        <p class="text-gray-100 mb-2">Harvesting Season: April - June</p>
+        <p class="text-gray-100 mb-2">Soil Type: Well-drained loamy soil</p>
+        <p class="text-gray-100 mb-2"> Wheat is the main cereal crop in India. The total area under the crop is about 29.8 million hectares in the country. The production of wheat in the country has increased significantly from 75.81 million MT in 2006-07 to an all time record high of 94.88 million MT in 2011-12. The productivity of wheat which was 2602 kg/hectare in 2004-05 has increased to 3140 kg/hectare in 2011-12. </p>
       </div>
     </div>
     <div class="">
-      <p class="text-gray-700">
+      <p class="text-gray-100">
         The major increase in the productivity of wheat has been observed in the states of Haryana, Punjab and Uttar Pradesh. Higher area coverage is reported from MP in recent years.Indian wheat is largely a soft/medium hard, medium protein, white bread wheat, somewhat similar to U.S. hard white wheat. Wheat grown in central and western India is typically hard, with high protein and high gluten content. India also produces around 1.0-1.2 million tons of durum wheat, mostly in the state of Madhya Pradesh. Most Indian durum is not marketed separately due to segregation problems in the market yards. However, some quantities are purchased by the private trade at a price premium, mainly for processing of higher value/branded products.
       </p>
     </div>
@@ -31,12 +30,12 @@
   <div id="basmati" class="mt-8">
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div>
-        <h3 class="text-xl font-semibold mb-2">Crop Name: Rice</h3>
-        <p class="text-gray-700 mb-2">Variety: Basmati</p>
-        <p class="text-gray-700 mb-2">Planting Season: June - July</p>
-        <p class="text-gray-700 mb-2">Harvesting Season: October - November</p>
-        <p class="text-gray-700 mb-2">Soil Type: Well-drained loamy soil</p>
-        <p class="text-gray-700 mb-2">Rice is a staple food for a significant portion of the world's population. It is the seed of the grass species
+        <h3 class="text-xl font-semibold mb-2 text-white">Crop Name: Rice</h3>
+        <p class="text-gray-100 mb-2">Variety: Basmati</p>
+        <p class="text-gray-100 mb-2">Planting Season: June - July</p>
+        <p class="text-gray-100 mb-2">Harvesting Season: October - November</p>
+        <p class="text-gray-100 mb-2">Soil Type: Well-drained loamy soil</p>
+        <p class="text-gray-100 mb-2">Rice is a staple food for a significant portion of the world's population. It is the seed of the grass species
           Oryza sativa. Basmati rice is known for its long grains and unique fragrance. It is typically grown in regions
           with warm temperatures and adequate water supply. The variability of composition and characteristics of rice is really broad and depends on variety and environmental conditions under which the crop is grown. In husked rice, protein content ranges in between 7per cent to 12per cent. The use of nitrogen fertilizers increases the percentage content of some amino acids.</p>
       </div>
@@ -45,7 +44,7 @@
       </div>
     </div>
     <div class="">
-      <p class="text-gray-700">
+      <p class="text-gray-100">
         India is an important centre of rice cultivation. The rice is cultivated on the largest areas in India. Historians believe that while the indica variety of rice was first domesticated in the area covering the foothills of the Eastern Himalayas (i.e. north-eastern India), stretching through Burma, Thailand, Laos, Vietnam and Southern China, the japonica variety was domesticated from wild rice in southern China which was introduced to India. Perennial wild rice still grow in Assam and Nepal. It seems to have appeared around 1400 BC in southern India after its domestication in the northern plains. It then spread to all the fertile alluvial plains watered by rivers. Some says that the word rice is derived from the Tamil word arisi.
       </p>
     </div>
@@ -58,16 +57,16 @@
         <img src="https://images.pexels.com/photos/15291100/pexels-photo-15291100/free-photo-of-close-up-photo-of-natural-tassel-fringe.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Crop Image" class="w-full h-80 mb-4 rounded-lg">
       </div>
       <div>
-        <h3 class="text-xl font-semibold mb-2">Crop Name: Jute</h3>
-        <p class="text-gray-700 mb-2">Variety: Corchorus capsularis (White Jute)</p>
-        <p class="text-gray-700 mb-2">Planting Season: March - April</p>
-        <p class="text-gray-700 mb-2">Harvesting Season: July - August</p>
-        <p class="text-gray-700 mb-2">Soil Type: Well-drained sandy loam soil</p>
-        <p class="text-gray-700 mb-2">Jute is an important natural fibre crop in India next to cotton. In trade and industry, jute and mesta crop together known as raw jute as their uses are almost same. Raw jute plays an important role in the country’s economy. Raw jute was originally considered as a source of raw material for packaging industries only. But it has now emerged as a versatile raw material for diverse applications, such as, textile industries, paper industries, building and automotive industries, use as soil saver, use as decorative and furnishing materials, etc. Raw jute being bio-degradable and annually renewable source,</p>
+        <h3 class="text-xl font-semibold mb-2 text-white">Crop Name: Jute</h3>
+        <p class="text-gray-100 mb-2">Variety: Corchorus capsularis (White Jute)</p>
+        <p class="text-gray-100 mb-2">Planting Season: March - April</p>
+        <p class="text-gray-100 mb-2">Harvesting Season: July - August</p>
+        <p class="text-gray-100 mb-2">Soil Type: Well-drained sandy loam soil</p>
+        <p class="text-gray-100 mb-2">Jute is an important natural fibre crop in India next to cotton. In trade and industry, jute and mesta crop together known as raw jute as their uses are almost same. Raw jute plays an important role in the country’s economy. Raw jute was originally considered as a source of raw material for packaging industries only. But it has now emerged as a versatile raw material for diverse applications, such as, textile industries, paper industries, building and automotive industries, use as soil saver, use as decorative and furnishing materials, etc. Raw jute being bio-degradable and annually renewable source,</p>
       </div>
     </div>
     <div class="">
-      <p class="text-gray-700">
+      <p class="text-gray-100">
         it is considered as an environment-friendly crop and it helps in the maintenance of the environment and ecological balance. Further attraction of Jute lies in its easy availability, inexhaustible quantity at a comparatively cheaper rate. Moreover, it can easily be blended with other natural and manmade fibres.<br>
         Jute cultivation is mainly concentrated in the eastern and north eastern India while that of mesta cultivation is spread almost throughout the country. The crop can be grown in low, medium and high land situation, both moisture stress and water stagnating condition. White Jute can be grown comparatively in low land situation while that of Tossa Jute prefers medium and high land situation. Mesta is grown in almost all over the country. It being a hardy crop and can tolerate moisture stress; its cultivation is spread in different agro-climatic situations unlike jute which is concentrated only in eastern and north eastern States. In a major part of Jute area, particularly in North Bengal, Bihar and North-Eastern States, the soil is acidic in nature. However, the Jute crop is growing in these areas in existing situation adjusting the crop sequence.
       </p>
@@ -78,12 +77,12 @@
   <div class="mt-8">
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div>
-        <h3 class="text-xl font-semibold mb-2">Crop Name: Sugarcane</h3>
-        <p class="text-gray-700 mb-2">Variety: Saccharum officinarum</p>
-        <p class="text-gray-700 mb-2">Planting Season: October - November</p>
-        <p class="text-gray-700 mb-2">Harvesting Season: December - March</p>
-        <p class="text-gray-700 mb-2">Soil Type: Well-drained fertile soil</p>
-        <p class="text-gray-700 mb-2">Sugarcane is a tall tropical grass that is grown for its thick stems, which are a rich source of sugar.
+        <h3 class="text-xl font-semibold mb-2 text-white">Crop Name: Sugarcane</h3>
+        <p class="text-gray-100 mb-2">Variety: Saccharum officinarum</p>
+        <p class="text-gray-100 mb-2">Planting Season: October - November</p>
+        <p class="text-gray-100 mb-2">Harvesting Season: December - March</p>
+        <p class="text-gray-100 mb-2">Soil Type: Well-drained fertile soil</p>
+        <p class="text-gray-100 mb-2">Sugarcane is a tall tropical grass that is grown for its thick stems, which are a rich source of sugar.
           It is one of the most important crops for sugar and bioenergy production. Sugarcane is typically harvested
           and processed to produce sugar, molasses, and ethanol.Sugarcane (Saccharum officinarum) family Gramineae (Poaceae) is widely grown crop in India. It provides employment to over a million people directly or indirectly besides contributing significantly to the national exchequer. Sugarcane growing countries of the world lay between the latitude 36.7° north and 31.0° south of the equator extending from tropical to subtropical zones. Sugar cane originated in New Guinea where it has been known for </p>
       </div>
@@ -92,7 +91,7 @@
       </div>
     </div>
     <div class="">
-      <p class="text-gray-700">
+      <p class="text-gray-100">
         thousands of years. Sugar cane plants spread along human migration routes to Asia and the Indian subcontinent. Here it cross-bred with some wild sugar cane relatives to produce the commercial sugar cane we know today. <br>
         Cultivation of sugarcane in India dates back to the Vedic period. The earliest mention of sugarcane cultivation is found in Indian writings of the period 1400 to 1000 B.C. It is now widely accepted that India is the original home of Saccharum species. Saccharum barberi and Polynesian group of island especially New Guinea is the centre of origin of S. officinarum. It belongs to family Gramineae (Poaceae), class monocotyledons and order glumaceae sub family panicoidae, tribe Andripogoneae and sub tribe saccharininea. The cultivated canes belong to two main groups: (a) thin, hardy north Indian types S.barberi and S.Sinense and (b) thick, juicy noble canes Saccharum officinarum. Highly prized cane is S. officinarum.
       </p>
