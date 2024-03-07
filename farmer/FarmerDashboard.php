37c3156871ea7a00d877b9583b4c30371bf60111
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2024-03-07 11:57:41 +0530

    farmerdashboard

--- farmer/FarmerDashboard.php
+++ farmer/FarmerDashboard.php
@@ -13,6 +13,7 @@ if ($_SESSION["farmer"] == false) {
     <title>Farmer Dashboard</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="../dist/output.css">
+    <link rel="stylesheet" href="../tailwind.css">
     <script src="../src/Js/code.js"></script>
 </head>
 
