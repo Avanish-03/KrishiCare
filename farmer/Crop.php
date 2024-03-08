f8b9c205283ed7fd253234970617ee531fc6f128
Dwivedi <devweb910@gmail.com>
2024-03-08 16:24:18 +0530

    Done

--- farmer/Crop.php
+++ farmer/Crop.php
@@ -136,7 +136,6 @@
           <img src="https://images.pexels.com/photos/2589457/pexels-photo-2589457.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Crop Image" class="w-full h-80 mb-4 rounded-lg">
         </div>
       </div>
-<<<<<<< HEAD
       <div class="">
         <p class="">
           India is an important centre of rice cultivation. The rice is cultivated on the largest areas in India. Historians believe that while the indica variety of rice was first domesticated in the area covering the foothills of the Eastern Himalayas (i.e. north-eastern India), stretching through Burma, Thailand, Laos, Vietnam and Southern China, the japonica variety was domesticated from wild rice in southern China which was introduced to India. Perennial wild rice still grow in Assam and Nepal. It seems to have appeared around 1400 BC in southern India after its domestication in the northern plains. It then spread to all the fertile alluvial plains watered by rivers. Some says that the word rice is derived from the Tamil word arisi.
@@ -164,22 +163,6 @@
           Jute cultivation is mainly concentrated in the eastern and north eastern India while that of mesta cultivation is spread almost throughout the country. The crop can be grown in low, medium and high land situation, both moisture stress and water stagnating condition. White Jute can be grown comparatively in low land situation while that of Tossa Jute prefers medium and high land situation. Mesta is grown in almost all over the country. It being a hardy crop and can tolerate moisture stress; its cultivation is spread in different agro-climatic situations unlike jute which is concentrated only in eastern and north eastern States. In a major part of Jute area, particularly in North Bengal, Bihar and North-Eastern States, the soil is acidic in nature. However, the Jute crop is growing in these areas in existing situation adjusting the crop sequence.
         </p>
       </div>
-=======
-      <div>
-        <h3 class="text-xl font-semibold mb-2 dark:text-white">Crop Name: Jute</h3>
-        <p class="dark:text-gray-100 mb-2">Variety: Corchorus capsularis (White Jute)</p>
-        <p class="dark:text-gray-100 mb-2">Planting Season: March - April</p>
-        <p class="dark:text-gray-100 mb-2">Harvesting Season: July - August</p>
-        <p class="dark:text-gray-100 mb-2">Soil Type: Well-drained sandy loam soil</p>
-        <p class="dark:text-gray-100 mb-2">Jute is an important natural fibre crop in India next to cotton. In trade and industry, jute and mesta crop together known as raw jute as their uses are almost same. Raw jute plays an important role in the country’s economy. Raw jute was originally considered as a source of raw material for packaging industries only. But it has now emerged as a versatile raw material for diverse applications, such as, textile industries, paper industries, building and automotive industries, use as soil saver, use as decorative and furnishing materials, etc. Raw jute being bio-degradable and annually renewable source,</p>
-      </div>
-    </div>
-    <div class="">
-      <p class="dark:text-gray-100">
-        it is considered as an environment-friendly crop and it helps in the maintenance of the environment and ecological balance. Further attraction of Jute lies in its easy availability, inexhaustible quantity at a comparatively cheaper rate. Moreover, it can easily be blended with other natural and manmade fibres.<br>
-        Jute cultivation is mainly concentrated in the eastern and north eastern India while that of mesta cultivation is spread almost throughout the country. The crop can be grown in low, medium and high land situation, both moisture stress and water stagnating condition. White Jute can be grown comparatively in low land situation while that of Tossa Jute prefers medium and high land situation. Mesta is grown in almost all over the country. It being a hardy crop and can tolerate moisture stress; its cultivation is spread in different agro-climatic situations unlike jute which is concentrated only in eastern and north eastern States. In a major part of Jute area, particularly in North Bengal, Bihar and North-Eastern States, the soil is acidic in nature. However, the Jute crop is growing in these areas in existing situation adjusting the crop sequence.
-      </p>
->>>>>>> eb0e1a0e0245704c4cf742c714a8b649380fe15f
     </div>
     <!--4. Sugarcane Crop Information -->
     <div class="mt-8 text-black dark:text-gray-300">
@@ -249,146 +232,4 @@
   </section> -->
   <div class="h-7"></div>
 
-<<<<<<< HEAD
 </section>
-=======
-  <!--4. Sugarcane Crop Information -->
-  <div class="mt-8">
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-      <div>
-        <h3 class="text-xl font-semibold mb-2 dark:text-white">Crop Name: Sugarcane</h3>
-        <p class="dark:text-gray-100 mb-2">Variety: Saccharum officinarum</p>
-        <p class="dark:text-gray-100 mb-2">Planting Season: October - November</p>
-        <p class="dark:text-gray-100 mb-2">Harvesting Season: December - March</p>
-        <p class="dark:text-gray-100 mb-2">Soil Type: Well-drained fertile soil</p>
-        <p class="dark:text-gray-100 mb-2">Sugarcane is a tall tropical grass that is grown for its thick stems, which are a rich source of sugar.
-          It is one of the most important crops for sugar and bioenergy production. Sugarcane is typically harvested
-          and processed to produce sugar, molasses, and ethanol.Sugarcane (Saccharum officinarum) family Gramineae (Poaceae) is widely grown crop in India. It provides employment to over a million people directly or indirectly besides contributing significantly to the national exchequer. Sugarcane growing countries of the world lay between the latitude 36.7° north and 31.0° south of the equator extending from tropical to subtropical zones. Sugar cane originated in New Guinea where it has been known for </p>
-      </div>
-      <div>
-        <img src="https://media.istockphoto.com/id/93541349/photo/sugar-cane-plantation.jpg?b=1&s=612x612&w=0&k=20&c=lBKJHxZccFdlK8-MF0YhsQG3nmwbZ8vPOF05VIV8-3o=" alt="Crop Image" class="w-full h-80 mb-4 rounded-lg">
-      </div>
-    </div>
-    <div class="">
-      <p class="dark:text-gray-100">
-        thousands of years. Sugar cane plants spread along human migration routes to Asia and the Indian subcontinent. Here it cross-bred with some wild sugar cane relatives to produce the commercial sugar cane we know today. <br>
-        Cultivation of sugarcane in India dates back to the Vedic period. The earliest mention of sugarcane cultivation is found in Indian writings of the period 1400 to 1000 B.C. It is now widely accepted that India is the original home of Saccharum species. Saccharum barberi and Polynesian group of island especially New Guinea is the centre of origin of S. officinarum. It belongs to family Gramineae (Poaceae), class monocotyledons and order glumaceae sub family panicoidae, tribe Andripogoneae and sub tribe saccharininea. The cultivated canes belong to two main groups: (a) thin, hardy north Indian types S.barberi and S.Sinense and (b) thick, juicy noble canes Saccharum officinarum. Highly prized cane is S. officinarum.
-      </p>
-    </div>
-  </div>
-
-</section>
-
-<!-- <section class="text-gray-600 body-font">
-  <div class="container px-5 py-10 mx-auto flex flex-wrap">
-     <div class="flex w-full mb-20 flex-wrap">
-      <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">Master Cleanse Reliac Heirloom</h1>
-      <p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.</p>
-    </div>
-    <div class="flex flex-wrap md:-m-2 -m-1">
-      <div class="flex flex-wrap w-1/2">
-        <div class="md:p-2 p-1 w-1/2">
-          <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://images.pexels.com/photos/1382102/pexels-photo-1382102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
-        </div>
-        <div class="md:p-2 p-1 w-1/2">
-          <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://images.pexels.com/photos/533982/pexels-photo-533982.jpeg?auto=compress&cs=tinysrgb&w=600">
-        </div>
-        <div class="md:p-2 p-1 w-full">
-          <img alt="gallery" class="w-full h-full object-cover object-center block" src="https://images.pexels.com/photos/221016/pexels-photo-221016.jpeg?auto=compress&cs=tinysrgb&w=600">
-        </div>
-      </div>
-      <div class="flex flex-wrap w-1/2">
-        <div class="md:p-2 p-1 w-full">
-          <img alt="gallery" class="w-full h-full object-cover object-center block" src="https://images.pexels.com/photos/289334/pexels-photo-289334.jpeg?auto=compress&cs=tinysrgb&w=600">
-        </div>
-        <div class="md:p-2 p-1 w-1/2">
-          <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://images.pexels.com/photos/164504/pexels-photo-164504.jpeg?auto=compress&cs=tinysrgb&w=600">
-        </div>
-        <div class="md:p-2 p-1 w-1/2">
-          <img alt="gallery" class="w-full object-cover h-full object-center block" src="https://images.pexels.com/photos/163740/harvest-cereals-machines-agriculture-163740.jpeg?auto=compress&cs=tinysrgb&w=600">
-        </div>
-      </div>
-    </div>
-  </div>
-</section> -->
-
-<!-- <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto flex flex-wrap">
-    <div class="flex flex-wrap w-full">
-      <div class="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6">
-        <div class="flex relative pb-12">
-          <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
-          </div>
-          <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center dark:text-white relative z-10">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-              <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-            </svg>
-          </div>
-          <div class="flex-grow pl-4">
-            <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 1</h2>
-            <p class="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
-          </div>
-        </div>
-        <div class="flex relative pb-12">
-          <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
-          </div>
-          <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center dark:text-white relative z-10">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-            </svg>
-          </div>
-          <div class="flex-grow pl-4">
-            <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 2</h2>
-            <p class="leading-relaxed">Vice migas literally kitsch +1 pok pok. Truffaut hot chicken slow-carb health goth, vape typewriter.</p>
-          </div>
-        </div>
-        <div class="flex relative pb-12">
-          <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
-          </div>
-          <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center dark:text-white relative z-10">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-              <circle cx="12" cy="5" r="3"></circle>
-              <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
-            </svg>
-          </div>
-          <div class="flex-grow pl-4">
-            <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 3</h2>
-            <p class="leading-relaxed">Coloring book nar whal glossier master cleanse umami. Salvia +1 master cleanse blog taiyaki.</p>
-          </div>
-        </div>
-        <div class="flex relative pb-12">
-          <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-            <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
-          </div>
-          <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center dark:text-white relative z-10">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-              <circle cx="12" cy="7" r="4"></circle>
-            </svg>
-          </div>
-          <div class="flex-grow pl-4">
-            <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 4</h2>
-            <p class="leading-relaxed">VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk bespoke try-hard cliche palo santo offal.</p>
-          </div>
-        </div>
-        <div class="flex relative">
-          <div class="flex-shrink-0 w-10 h-10 rounded-full bg-indigo-500 inline-flex items-center justify-center dark:text-white relative z-10">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-              <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-              <path d="M22 4L12 14.01l-3-3"></path>
-            </svg>
-          </div>
-          <div class="flex-grow pl-4">
-            <h2 class="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">FINISH</h2>
-            <p class="leading-relaxed">Pitchfork ugh tattooed scenester echo park gastropub whatever cold-pressed retro.</p>
-          </div>
-        </div>
-      </div>
-      <img class="lg:w-3/5 md:w-1/2 object-cover object-center rounded-lg md:mt-0 mt-12" src="https://images.pexels.com/photos/326082/pexels-photo-326082.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="step">
-    </div>
-  </div>
-</section> -->
->>>>>>> eb0e1a0e0245704c4cf742c714a8b649380fe15f
