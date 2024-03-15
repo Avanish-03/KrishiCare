982d737b6c6066dc69a5e1817e3d24247e33ed6d
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2024-03-15 23:28:27 +0530

    farmer admin

--- admin/farmer.php
+++ admin/farmer.php
@@ -22,10 +22,13 @@
                         </div>
                         <div x-show="open" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" x-description="Background backdrop, show/hide based on modal state." class="fixed inset-0 flex items-center justify-center bg-gray-600 bg-opacity-75 transition-opacity" @keydown.escape.window="open = false">
                             <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:w-full sm:max-w-lg" @click.away="open = false">
-                                <div class="p-4 sm:p-10 text-center">
+                                <div class=" sm:p-10">
+                                    <button @click="open = false" type="button" class="px-2 flex justify-center items-end gap-2 rounded-md border border-transparent font-semibold text-black transition-all text-lg">
+                                        ×
+                                    </button>
+                                    <div class="md:col-span-4 text-4xl bg-slate-700 rounded-md px-2">Add New Farmer</div>
                                     <form id="farmForm" method="post" action="#">
-
-                                        <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-4">
+                                        <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-4 text-black font-bold">
                                             <div class="md:col-span-4">
                                                 <label for="firstname">First Name</label>
                                                 <input type="text" name="firstname" id="firstname" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" />
@@ -58,13 +61,11 @@
 
                                             <div class="md:col-span-2">
                                                 <label for="state">State</label>
-                                                <!-- <input type="text" name="state" id="state" class="state h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" placeholder="" /> -->
                                                 <select type="text" name="state" id="state" class="state h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" placeholder="" onchange="loadCities()"></select>
                                             </div>
 
                                             <div class="md:col-span-2">
                                                 <label for="city">City</label>
-                                                <!-- <input type="text" name="city" id="city" class="city h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" placeholder="" /> -->
                                                 <select type="text" name="city" id="city" class="city h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" placeholder=""></select>
                                             </div>
 
@@ -87,12 +88,9 @@
                                     </form>
 
                                     <div class="mt-6 flex justify-center gap-x-4">
-                                        <a class="py-2.5 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm" href="javascript:;">
-                                            Sign out
+                                        <a class="py-2 w-full inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-green-700 text-gray-100 shadow-sm align-middle hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-700 focus:ring-green-600 transition-all text-lg" href="javascript:;">
+                                            Register
                                         </a>
-                                        <button @click="open = false" type="button" class="py-2.5 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm">
-                                            Cancel
-                                        </button>
                                     </div>
                                 </div>
                             </div>
