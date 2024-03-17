c85b45c36a62a8854e45900bbc61a0a04365b861
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2024-03-17 14:38:42 +0530

    new changes

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,12 +1,10 @@
-<?php session_start(); ?>
+<?php //session_start(); ?>
 <!-- dashboard -->
 <section class="h-full mx-auto p-8 rounded-md">
     <h1 class="text-3xl font-bold text-black dark:text-gray-300">Dashboard</h1>
-    <div class="bg-gray-200 dark:bg-slate-600 w-full h-48 rounded my-2 px-8 flex items-center relative">
-        <div class="w-fit h-full flex justify-start">
+    <div class="bg-gray-200 dark:bg-slate-600 w-full h-48 rounded my-2 px-8 flex items-center ">
+        <div class=" h-full flex justify-">
             <img class="h-full w-full" src="../img/Hello-rafiki.png" alt="">
-        </div>
-        <div class="h-full">
             <div class="h-5 w-[45%] blur-2xl shadow-2xl"></div>
             <h1 class="text-7xl h-full w-full select-none z-10 font-extrabold flex justify-center items-center bg-cover bg-no-repeat bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-gray-400">Admin </h1>
         </div>
--- admin/profile.php
+++ admin/profile.php
@@ -1,3 +1,31 @@
+<?php
+include('../Backend/config.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['admin_img'])) {
+    // Check if a new image is provided
+    if ($_FILES['admin_img']['name'] != '') {
+        $newImageName = $_FILES['admin_img']['name'];
+        $newImageTemp = $_FILES['admin_img']['tmp_name'];
+        $uploadPath = '../img/admin_img/'; // Corrected upload path
+        // Move the uploaded image to the desired directory
+        if (move_uploaded_file($newImageTemp, $uploadPath . $newImageName)) {
+            // Update data in the database with the new image
+            $query = "UPDATE admin SET admin_img='$uploadPath$newImageName' WHERE name ='{$_SESSION['user']}'"; // Corrected query
+            $details = mysqli_query($con, $query);
+            if ($details) {
+                echo "<script>alert('Cover Image updated successfully');</script>";
+                echo "<script>window.location.href='Hospital_dasboard.php';</script>";
+                exit();
+            } else {
+                echo "<script>alert('Error updating database: " . mysqli_error($con) . "');</script>";
+            }
+        } else {
+            echo "<script>alert('Error uploading file. Check if the directory has the correct permissions');</script>";
+        }
+    }
+}
+?>
+
 <!-- Profile Section -->
 <section class="w-full h-full mx-auto p-8 rounded-md">
     <!-- <h1 class="text-3xl font-bold mb-4 text-black dark:text-gray-300">Profile</h1> -->
@@ -18,41 +46,28 @@
                     <div class="px-6">
                         <div class="flex flex-wrap justify-center">
                             <div class="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-                                <div class="relative">
-                                    <div class="relative flex flex-col items-center justify-center" x-data="{ open: false }">
-                                        <div class="">
-                                            <button @click="open = true" class="bg-green-600 hover:bg-green-500 text-white font-bold py-2 px-4 rounded-md shadow-md transition duration-300 ease-in-out">
-                                                <img alt="..." src="../img/avanish.jpg" class="shadow-xl rounded-full w-32 h-32 align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px">
-                                            </button>
+                                <div x-data="{photoName: null, photoPreview: null}" class="col-span-6 ml-2 sm:col-span-4 md:mr-3">
+                                    <!-- Photo File Input -->
+                                    <input type="file" class="hidden" x-ref="photo" x-on:change="photoName = $refs.photo.files[0].name; const reader = new FileReader(); reader.onload = (e) => {photoPreview = e.target.result;}; reader.readAsDataURL($refs.photo.files[0]);">
+                                    <div class="text-center">
+                                        <!-- Current Profile Photo -->
+                                        <div class="-mt-20" x-show="! photoPreview">
+                                            <img src="../img/admin_img/ <?php echo "" ?>" class="w-40 h-40 m-auto rounded-full shadow">
                                         </div>
-                                        <div x-show="open" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" x-description="Background backdrop, show/hide based on modal state." class="fixed inset-0 flex items-center justify-center bg-gray-600 bg-opacity-75 transition-opacity" @keydown.escape.window="open = false">
-                                            <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:w-full sm:max-w-lg" @click.away="open = false">
-                                                <div class="sm:p-10">
-                                                    <button @click="open = false" type="button" class="px-2 flex justify-center items-end gap-2 rounded-md border border-transparent font-semibold text-black transition-all text-lg">
-                                                        ×
-                                                    </button>
-                                                    <div class="md:col-span-4 text-4xl bg-slate-700 rounded-md px-2"></div>
-                                                    <form method="post" enctype="multipart/form-data">
-                                                        <hr class="w-full border-2">
-                                                        <div class="flex justify-center items-center text-lg font-bold pt-4">
-                                                            <label for="image" class="text-black">Upload Profile Image</label>
-                                                        </div>
-                                                        <div class="px-10 py-2">
-                                                            <input type="file" name="adminimg" id="new_image" class="border text-black border-gray-300 rounded-md px-2 w-full h-10 mt-1">
-                                                        </div>
-                                                        <div class="px-10 py-2">
-                                                            <button type="submit" name="admin_pic" class="border border-gray-300 bg-gray-600 hover:bg-gray-700 text-white rounded-md px-2 w-full h-10 mt-1">Upload</button>
-                                                        </div>
-                                                    </form>
-                                                </div>
-                                            </div>
+                                        <!-- New Profile Photo Preview -->
+                                        <div class="-mt-20" x-show="photoPreview" style="display: none;">
+                                            <span class="block w-40 h-40 rounded-full m-auto shadow" x-bind:style="'background-size: cover; background-repeat: no-repeat; background-position: center center; background-image: url(\'' + photoPreview + '\');'" style="background-size: cover; background-repeat: no-repeat; background-position: center center; background-image: url('null');">
+                                            </span>
                                         </div>
+                                        <button type="submit" class="inline-flex items-center px-4 py-2 bg-green-600 border border-gray-300 rounded-md font-semibold text-xs text-gray-100 uppercase tracking-widest shadow-sm hover:text-gray-100 focus:outline-none focus:border-green-400 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150 mt-2 ml-3" x-on:click.prevent="$refs.photo.click()">
+                                           Edit
+                                        </button>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="text-center mt-20 text-white">
+                    <div class="text-center mt-10 text-white">
                         <h3 class="text-4xl font-semibold leading-normal  mb-2">
                             Avanish Yadav
                         </h3>
@@ -87,30 +102,3 @@
     </div>
 </section>
 
-<?php
-include('../Backend/config.php');
-
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['admin_imgs'])) {
-    // Check if a new image is provided
-    if ($_FILES['admin_img']['name'] != '') {
-        $newImageName = $_FILES['admin_img']['name'];
-        $newImageTemp = $_FILES['admin_img']['tmp_name'];
-        $uploadPath = '../img/admin_img/'; // Corrected upload path
-        // Move the uploaded image to the desired directory
-        if (move_uploaded_file($newImageTemp, $uploadPath . $newImageName)) {
-            // Update data in the database with the new image
-            $query = "UPDATE admin SET admin_img='$uploadPath$newImageName' WHERE name ='{$_SESSION['user']}'"; // Corrected query
-            $details = mysqli_query($con, $query);
-            if ($details) {
-                echo "<script>alert('Cover Image updated successfully');</script>";
-                echo "<script>window.location.href='Hospital_dasboard.php';</script>";
-                exit();
-            } else {
-                echo "<script>alert('Error updating database: " . mysqli_error($con) . "');</script>";
-            }
-        } else {
-            echo "<script>alert('Error uploading file. Check if the directory has the correct permissions');</script>";
-        }
-    }
-}
-?>
