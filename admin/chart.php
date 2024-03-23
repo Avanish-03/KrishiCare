d44eb7b660f33cd1af31319e262ebdc675985a58
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2024-03-23 12:06:20 +0530

    error dekhovna chart me

---
+++ admin/chart.php
@@ -0,0 +1,32 @@
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities',
+          is3D: true,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div class="border-2 border-gray-500">
+        <div id="piechart_3d" style="width: 900px; height: 500px;"></div>
+    </div>
+  </body>
+</html>
--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,14 +1,18 @@
 <?php session_start(); ?>
 
 <!-- dashboard -->
-<section class="h-full w-full mx-auto p-8 rounded-md">
+<section class="h-full mx-auto p-8 rounded-md">
     <h1 class="text-3xl font-bold mb-4 text-black dark:text-gray-300 duration-700">Dashboard</h1>
     <div class="bg-gray-200 dark:bg-slate-600 w-full h-32 rounded my-2 px-8 flex items-center">
-        <div class="h-full flex justify-">
+        <div class="h-full flex justify-center">
             <img class="h-full w-full" src="../img/Hello-rafiki.png" alt="">
             <div class="h-5 w-[45%] blur-2xl shadow-2xl"></div>
-            <h1 class="text-xl sm:text-5xl md:text-7xl h-full w-full select-none z-10 font-extrabold flex justify-center items-center bg-cover bg-no-repeat bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-gray-400">Admin</h1>
+            <h1 class="text-7xl h-full w-full select-none z-10 font-extrabold flex justify-center items-center bg-cover bg-no-repeat bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-gray-400">Admin</h1>
         </div>
     </div>
+   
     <div class="h-7"></div>
 </section>
+
+<?php 
+require_once("chart.php");?>
