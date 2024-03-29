05e6ae33df9dc129366610d7ce2c5e839b3382f7
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2024-03-29 09:07:27 +0530

    farmer setting and notyfication 28/03/24

--- admin/Notification.php
+++ admin/Notification.php
@@ -1,7 +1,7 @@
 <!-- Notification Section -->
 <section class="w-full h-full mx-auto my-8 px-8 rounded-md">
     <h1 class="text-3xl font-bold mb-4 text-black dark:text-gray-300">Notification</h1>
-    <!-- information Section -->
+
     <div class="h-48 w-full bg-gray-200 rounded-lg dark:bg-slate-600 flex items-center">
         <div class="w-fit h-full flex justify-start px-8">
             <img class="h-full w-full " src="../img/New notifications-bro.png" alt="">
@@ -10,17 +10,69 @@
             <h1 class=" text-4xl font-bold">Notification</h1>
         </div>
     </div>
-    <div class="h-full w-full my-8 bg-gray-200 rounded-lg dark:bg-slate-600 grid grid-cols-1 md:grid-cols-2">
-        <div class="bg-yellow-400 h-full w-full rounded-lg">
-            
+    <div class="h-fit w-full bg-gray-200 dark:bg-gray-600 my-8 rounded-md">
+        <div class="px-12 py-12 ">
+            <h1 class="text-3xl font-bold">Notification Settings</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, cumque.</p>
+            <hr class="border-1 my-8 border-gray-500">
+
+            <h1 class="text-xl font-bold">Comments</h1>
+            <div class="flex justify-between mr-8">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, cumque.</p>
+                <div class="flex flex-col mt-8">
+                    <button class="flex justify-center items-center my-1">
+                        <div class="bg-gray-400 h-5 w-9 rounded-xl mr-2 ">
+                            <p class="h-5 w-5 rounded-full bg-white"></p>
+                        </div>
+                        Push
+                    </button>
+                    <button class="flex justify-center items-center my-1">
+                        <div class="bg-green-500 h-5 w-9 rounded-xl mr-2 ">
+                            <p class="h-5 w-5 rounded-full bg-white ml-4"></p>
+                        </div>
+                        Email
+                    </button>
+                    <button class="flex justify-center items-center my-1">
+                        <div class="bg-gray-400 h-5 w-9 rounded-xl mr-2 ">
+                            <p class="h-5 w-5 rounded-full bg-white "></p>
+                        </div>
+                        SMS
+                    </button>
+                </div>
+            </div>
+            <hr class="border-1 my-8 border-gray-500">
+
+            <h1 class="text-xl font-bold">Updates</h1>
+            <div class="flex justify-between mr-8">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, cumque.</p>
+                <div class="flex flex-col mt-8">
+                    <button class="flex justify-center items-center my-1">
+                        <div class="bg-gray-400 h-5 w-9 rounded-xl mr-2 ">
+                            <p class="h-5 w-5 rounded-full bg-white"></p>
+                        </div>
+                        Push
+                    </button>
+                    <button class="flex justify-center items-center my-1">
+                        <div class="bg-green-500 h-5 w-9 rounded-xl mr-2 ">
+                            <p class="h-5 w-5 rounded-full bg-white ml-4"></p>
+                        </div>
+                        Email
+                    </button>
+                    <button class="flex justify-center items-center my-1">
+                        <div class="bg-green-500 h-5 w-9 rounded-xl mr-2 ">
+                            <p class="h-5 w-5 rounded-full bg-white ml-4"></p>
+                        </div>
+                        SMS
+                    </button>
+                </div>
+            </div>
+            <hr class="border-1 my-8 border-gray-500">
+
+
+            <h1 class="text-xl font-bold">Teams & Conditions</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, cumque.</p>
         </div>
-        <div class="bg-yellow-500 h-full w-full rounded-lg"></div>
     </div>
-    <div class="my-8 h-full w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        <div class="w-full h-full bg-gray-200 rounded-lg dark:bg-slate-600"></div>
-        <div class="w-full h-full bg-gray-200 rounded-lg dark:bg-slate-600"></div>
-        <div class="w-full h-full bg-gray-200 rounded-lg dark:bg-slate-600"></div>
-        <div class="w-full h-full bg-gray-200 rounded-lg dark:bg-slate-600"></div>
-    </div>
+
     <div class="h-7"></div>
 </section>
