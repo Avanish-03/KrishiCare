59ee1c9bf6963f0e5f6f4035612da964fdc4f4b0
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2024-03-30 23:00:42 +0530

    report page

--- admin/report.php
+++ admin/report.php
@@ -4,115 +4,29 @@
     <!-- information Section -->
     <div class="h-48 w-full bg-gray-200 rounded-lg dark:bg-slate-600 flex items-center">
         <div class="w-fit h-full flex justify-start px-8">
-            <img class="h-full w-full" src="../img/Report-illustrater.png" alt="">
+            <img class="h-full w-full" src="../img/reqest-data.png" alt="">
         </div>
         <div class="w-[50%] h-full flex items-center">
             <h1 class=" text-4xl font-bold">Reports</h1>
         </div>
     </div>
     <div class="my-8 h-full w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        <div class="w-full h-full bg-gray-200 rounded-lg dark:bg-slate-600">
-            <!-- component -->
-            <!-- This is an example component -->
+        <div class="w-fit h-fit rounded-lg">
             <div class="max-w-2xl mx-auto">
-
-                <div class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+                <div class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-slate-600 dark:border-slate-700">
                     <a href="#">
-                        <img class="rounded-t-lg" src="https://img.freepik.com/free-photo/hand-holding-writing-checklist-application-form-document-clipboard-white-background-3d-illustration_56104-1551.jpg?t=st=1710697177~exp=1710700777~hmac=ab66405f0c3a36efa7c5d316f16ae6197a44e3e8eac8e1bca62f744189b15d18&w=740" alt="">
+                        <!-- <img class="rounded-t-lg"
+                            src="https://img.freepik.com/free-photo/hand-holding-writing-checklist-application-form-document-clipboard-white-background-3d-illustration_56104-1551.jpg?t=st=1710697177~exp=1710700777~hmac=ab66405f0c3a36efa7c5d316f16ae6197a44e3e8eac8e1bca62f744189b15d18&w=740"
+                            alt=""> -->
                     </a>
                     <div class="p-5">
                         <a href="#">
-                            <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                            <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">Number of reports are published on krishicare and help to the farmers</h5>
                         </a>
-                        <p class="font-normal text-gray-700 mb-3 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Read more
-                            <svg class="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                            </svg>
-                        </a>
+                        <p class="font-normal text-gray-700 mb-3 dark:text-gray-200">Here are the report for the krish yadav. who have request for the test of soil </p>
+                        <a href="#" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Read more</a>
                     </div>
                 </div>
-
-                <p class="mt-5">This card component is part of a larger, open-source library of Tailwind CSS components. Learn more by going to the official <a class="text-blue-600 hover:underline" href="#" target="_blank">Flowbite Documentation</a>.</p>
-            </div>
-        </div>
-        <div class="w-full h-full bg-gray-200 rounded-lg dark:bg-slate-600">
-            <!-- component -->
-            <!-- This is an example component -->
-            <div class="max-w-2xl mx-auto">
-
-                <div class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="rounded-t-lg" src="https://img.freepik.com/free-photo/hand-holding-writing-checklist-application-form-document-clipboard-white-background-3d-illustration_56104-1551.jpg?t=st=1710697177~exp=1710700777~hmac=ab66405f0c3a36efa7c5d316f16ae6197a44e3e8eac8e1bca62f744189b15d18&w=740" alt="">
-                    </a>
-                    <div class="p-5">
-                        <a href="#">
-                            <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                        </a>
-                        <p class="font-normal text-gray-700 mb-3 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Read more
-                            <svg class="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-
-                <p class="mt-5">This card component is part of a larger, open-source library of Tailwind CSS components. Learn more by going to the official <a class="text-blue-600 hover:underline" href="#" target="_blank">Flowbite Documentation</a>.</p>
-            </div>
-        </div>
-        <div class="w-full h-full bg-gray-200 rounded-lg dark:bg-slate-600">
-            <!-- component -->
-            <!-- This is an example component -->
-            <div class="max-w-2xl mx-auto">
-
-                <div class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="rounded-t-lg" src="https://img.freepik.com/free-photo/hand-holding-writing-checklist-application-form-document-clipboard-white-background-3d-illustration_56104-1551.jpg?t=st=1710697177~exp=1710700777~hmac=ab66405f0c3a36efa7c5d316f16ae6197a44e3e8eac8e1bca62f744189b15d18&w=740" alt="">
-                    </a>
-                    <div class="p-5">
-                        <a href="#">
-                            <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                        </a>
-                        <p class="font-normal text-gray-700 mb-3 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Read more
-                            <svg class="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-
-                <p class="mt-5">This card component is part of a larger, open-source library of Tailwind CSS components. Learn more by going to the official <a class="text-blue-600 hover:underline" href="#" target="_blank">Flowbite Documentation</a>.</p>
-            </div>
-        </div>
-        <div class="w-full h-full bg-gray-200 rounded-lg dark:bg-slate-600">
-            <!-- component -->
-            <!-- This is an example component -->
-            <div class="max-w-2xl mx-auto">
-
-                <div class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="rounded-t-lg" src="https://img.freepik.com/free-photo/hand-holding-writing-checklist-application-form-document-clipboard-white-background-3d-illustration_56104-1551.jpg?t=st=1710697177~exp=1710700777~hmac=ab66405f0c3a36efa7c5d316f16ae6197a44e3e8eac8e1bca62f744189b15d18&w=740" alt="">
-                    </a>
-                    <div class="p-5">
-                        <a href="#">
-                            <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                        </a>
-                        <p class="font-normal text-gray-700 mb-3 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                        <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Read more
-                            <svg class="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-
-                <p class="mt-5">This card component is part of a larger, open-source library of Tailwind CSS components. Learn more by going to the official <a class="text-blue-600 hover:underline" href="#" target="_blank">Flowbite Documentation</a>.</p>
             </div>
         </div>
     </div>
