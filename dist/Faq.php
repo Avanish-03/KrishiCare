28fb591412131ff9110d9a5c93680ccb690b9aea
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2024-04-02 19:32:40 +0530

    faq

--- dist/Faq.php
+++ dist/Faq.php
@@ -1,17 +1,20 @@
-<?php include('Header.php'); ?>
+<?php include ('Header.php'); ?>
 <!-- component -->
 
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="https://cdn-icons-png.freepik.com/256/10341/10341413.png?ga=GA1.1.253096211.1707907143&semt=ais">
+    <link rel="icon"
+        href="https://cdn-icons-png.freepik.com/256/10341/10341413.png?ga=GA1.1.253096211.1707907143&semt=ais">
     <title>KrishiCare - Farmer Information System</title>
     <link rel="stylesheet" href="../dist/output.css">
     <link rel="stylesheet" href="../src/Js/styles.css">
     <script src="../src/Js/code.js"></script>
     <script src="../flowbite.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.3/tailwind.min.css" integrity="sha512-wl80ucxCRpLkfaCnbM88y4AxnutbGk327762eM9E/rRTvY/ZGAHWMZrYUq66VQBYMIYDFpDdJAOGSLyIPHZ2IQ==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.3/tailwind.min.css"
+        integrity="sha512-wl80ucxCRpLkfaCnbM88y4AxnutbGk327762eM9E/rRTvY/ZGAHWMZrYUq66VQBYMIYDFpDdJAOGSLyIPHZ2IQ=="
+        crossorigin="anonymous" />
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.js" defer></script>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
@@ -37,7 +40,8 @@
                             <div class="text-xs text-gray-700">See our FAQ for more details</div>
                             <div class=" text-gray-600 mt-8 lg:mr-16">
                                 <label for="" class="text-xl flex justify-center items-center hover">
-                                    🔎<input type="search" name="serch" placeholder="Search" class="bg-white w-full h-10 px-5 rounded-full text-sm focus:outline-green-600">
+                                    🔎<input type="search" name="serch" placeholder="Search"
+                                        class="bg-white w-full h-10 px-5 rounded-full text-sm focus:outline-green-600">
                                 </label>
                             </div>
                             <div>
@@ -49,77 +53,128 @@
                         <div class="p-4">
                             <div class="item px-6 py-6 " x-data="{isOpen : false}">
                                 <a href="#" class="flex items-center justify-between" @click.prevent="isOpen = true">
-                                    <h4 :class="{'text-green-400 font-medium' : isOpen == true}">what does this website ?</h4>
-                                    <svg :class="{'transform rotate-180' : isOpen == true}" class="w-5 h-5 text-gray-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                    <h4 :class="{'text-green-400 font-medium' : isOpen == true}">what does this website
+                                        ?</h4>
+                                    <svg :class="{'transform rotate-180' : isOpen == true}"
+                                        class="w-5 h-5 text-gray-500" fill="none" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
+                                        stroke="currentColor">
                                         <path d="M19 9l-7 7-7-7"></path>
                                     </svg>
                                 </a>
-                                <div x-show="isOpen" @click.away="isOpen = false" class="mt-3 text-justify" :class="{'text-gray-600' : isOpen == true}">
-                                    <p></p>
+                                <div x-show="isOpen" @click.away="isOpen = false" class="mt-3 text-justify"
+                                    :class="{'text-gray-600' : isOpen == true}">
+                                    <p>KrishiCare is a platform designed to support farmers in optimizing their
+                                        agricultural practices. It offers valuable insights into crop cultivation, pest
+                                        management, and soil health. The Virtual Laboratory Module enables farmers to
+                                        analyze data with expert guidance. weather forecasts. </p>
                                 </div>
                             </div>
 
                             <div class="item px-6 py-6" x-data="{isOpen : false}">
                                 <a href="#" class="flex items-center justify-between" @click.prevent="isOpen = true">
-                                    <h4 :class="{'text-green-400 font-medium' : isOpen == true}">How can we use Krishicare ?</h4>
-                                    <svg :class="{'transform rotate-180' : isOpen == true}" class="w-5 h-5 text-gray-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                    <h4 :class="{'text-green-400 font-medium' : isOpen == true}">How can we use
+                                        Krishicare ?</h4>
+                                    <svg :class="{'transform rotate-180' : isOpen == true}"
+                                        class="w-5 h-5 text-gray-500" fill="none" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
+                                        stroke="currentColor">
                                         <path d="M19 9l-7 7-7-7"></path>
                                     </svg>
                                 </a>
-                                <div x-show="isOpen" @click.away="isOpen = false" class="mt-3 text-justify" :class="{'text-gray-600' : isOpen == true}">
-                                    <p></p>
+                                <div x-show="isOpen" @click.away="isOpen = false" class="mt-3 text-justify"
+                                    :class="{'text-gray-600' : isOpen == true}">
+                                    <p>To use KrishiCare:<br>
+                                        1. Register on the website.<br>
+                                        2. Explore farming resources.<br>
+                                        3. Engage with the Virtual Laboratory for soil testing.<br>
+                                        5. Stay updated with weather and crop information.<br>
+                                        6. know about new technology in farming.<br>
+                                        7. Provide feedback for improvement.</p>
                                 </div>
                             </div>
 
                             <div class="item px-6 py-6" x-data="{isOpen : false}">
                                 <a href="#" class="flex items-center justify-between" @click.prevent="isOpen = true">
-                                    <h4 :class="{'text-green-400 font-medium' : isOpen == true}">How many types user suports ?</h4>
-                                    <svg :class="{'transform rotate-180' : isOpen == true}" class="w-5 h-5 text-gray-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                    <h4 :class="{'text-green-400 font-medium' : isOpen == true}">How many types user
+                                        suports ?</h4>
+                                    <svg :class="{'transform rotate-180' : isOpen == true}"
+                                        class="w-5 h-5 text-gray-500" fill="none" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
+                                        stroke="currentColor">
                                         <path d="M19 9l-7 7-7-7"></path>
                                     </svg>
                                 </a>
-                                <div x-show="isOpen" @click.away="isOpen = false" class="mt-3 text-justify" :class="{'text-gray-600' : isOpen == true}">
-                                    <p></p>
+                                <div x-show="isOpen" @click.away="isOpen = false" class="mt-3 text-justify"
+                                    :class="{'text-gray-600' : isOpen == true}">
+                                    <p>KrishiCare supports various user types involved in agriculture:<br>
+                                        1. Farmers <br>
+                                        2. Agricultural Experts <br>
+                                        3. Agribusinesses <br>
+                                        4. Government Agencies <br>
+                                        5. Researchers <br>
+                                        6. Students</p>
                                 </div>
                             </div>
 
                             <div class="item px-6 py-6" x-data="{isOpen : false}">
                                 <a href="#" class="flex items-center justify-between" @click.prevent="isOpen = true">
-                                    <h4 :class="{'text-green-400 font-medium' : isOpen == true}">How can i register on Krishicare ?</h4>
-                                    <svg :class="{'transform rotate-180' : isOpen == true}" class="w-5 h-5 text-gray-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                    <h4 :class="{'text-green-400 font-medium' : isOpen == true}">How can i register on
+                                        Krishicare ?</h4>
+                                    <svg :class="{'transform rotate-180' : isOpen == true}"
+                                        class="w-5 h-5 text-gray-500" fill="none" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
+                                        stroke="currentColor">
                                         <path d="M19 9l-7 7-7-7"></path>
                                     </svg>
                                 </a>
-                                <div x-show="isOpen" @click.away="isOpen = false" class="mt-3 text-justify" :class="{'text-gray-600' : isOpen == true}">
-                                    <p></p>
+                                <div x-show="isOpen" @click.away="isOpen = false" class="mt-3 text-justify"
+                                    :class="{'text-gray-600' : isOpen == true}">
+                                    <p>To register on KrishiCare: <br>
+                                        1. Visit the KrishiCare website. <br>
+                                        2. Click on the "Register" or "Sign Up" option. <br>
+                                        3. Fill out the registration form with your details. <br>
+                                        4. Verify your email address if required. <br>
+                                        5. Log in to your account. <br>
+                                        6. Start exploring the platform's features and resources.</p>
                                 </div>
                             </div>
 
                             <div class="item px-6 py-6" x-data="{isOpen : false}">
                                 <a href="#" class="flex items-center justify-between" @click.prevent="isOpen = true">
-                                    <h4 :class="{'text-green-400 font-medium' : isOpen == true}">My data will be secure or not ?</h4>
-                                    <svg :class="{'transform rotate-180' : isOpen == true}" class="w-5 h-5 text-gray-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                    <h4 :class="{'text-green-400 font-medium' : isOpen == true}">My data will be secure
+                                        or not ?</h4>
+                                    <svg :class="{'transform rotate-180' : isOpen == true}"
+                                        class="w-5 h-5 text-gray-500" fill="none" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
+                                        stroke="currentColor">
                                         <path d="M19 9l-7 7-7-7"></path>
                                     </svg>
                                 </a>
-                                <div x-show="isOpen" @click.away="isOpen = false" class="mt-3 text-justify" :class="{'text-gray-600' : isOpen == true}">
-                                    <p></p>
+                                <div x-show="isOpen" @click.away="isOpen = false" class="mt-3 text-justify"
+                                    :class="{'text-gray-600' : isOpen == true}">
+                                    <p>"Absolutely, your data security is of utmost importance to us at KrishiCare.
+                                        We've implemented a range of measures to ensure that your information remains safe and protected. From stringent access controls to regular data backups, encryption protocols, and compliance with data protection regulations, we've got you covered. You can rest assured that your data is in good hands with us."</p>
                                 </div>
                             </div>
 
-                            <div class="item px-6 py-6" x-data="{isOpen : false}">
+                            <!-- <div class="item px-6 py-6" x-data="{isOpen : false}">
                                 <a href="#" class="flex items-center justify-between" @click.prevent="isOpen = true">
                                     <h4 :class="{'text-green-400 font-medium' : isOpen == true}">Question 6 ?</h4>
-                                    <svg :class="{'transform rotate-180' : isOpen == true}" class="w-5 h-5 text-gray-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                    <svg :class="{'transform rotate-180' : isOpen == true}"
+                                        class="w-5 h-5 text-gray-500" fill="none" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
+                                        stroke="currentColor">
                                         <path d="M19 9l-7 7-7-7"></path>
                                     </svg>
                                 </a>
-                                <div x-show="isOpen" @click.away="isOpen = false" class="mt-3 text-justify" :class="{'text-gray-600' : isOpen == true}">
+                                <div x-show="isOpen" @click.away="isOpen = false" class="mt-3 text-justify"
+                                    :class="{'text-gray-600' : isOpen == true}">
                                     <p></p>
                                 </div>
-                            </div>
-                        </div>
+                            </div> -->
 
+                        </div>
                     </div>
                 </div>
             </div>
@@ -128,7 +183,9 @@
 
     </article>
 
-    <div x-data="scrollHandler(document.getElementById('the-article'))" x-cloak aria-hidden="true" @scroll.window="calculateHeight(window.scrollY)" class="fixed h-screen w-1 hover:bg-gray-200 top-0 left-0 bg-gray-300">
+    <div x-data="scrollHandler(document.getElementById('the-article'))" x-cloak aria-hidden="true"
+        @scroll.window="calculateHeight(window.scrollY)"
+        class="fixed h-screen w-1 hover:bg-gray-200 top-0 left-0 bg-gray-300">
         <div :style="`max-height:${height}%`" class="h-full bg-green-400"></div>
     </div>
 
@@ -260,4 +317,4 @@
         </div>
     </div>
 </section> -->
-<?php include('Footer.php'); ?>
+<?php include ('Footer.php'); ?>
